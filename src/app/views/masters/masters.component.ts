9f1f634d39083ceb9ced7f59abfecebd40b2910d
ajaybhasy-experion <ajay.bhasy@experionglobal.com>
2019-03-12 09:52:33 +0530

    {s3s} Added Edit option for branch https://trello.com/c/MZAZA8Oa

--- src/app/views/masters/masters.component.ts
+++ src/app/views/masters/masters.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { ModalDirective } from 'ngx-bootstrap/modal';
-import { FormGroup, FormControl ,Validators} from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Branch } from '../../models/branch';
 import { Course } from '../../models/course';
 import { ApiService } from '../../services/api.service';
@@ -31,29 +31,29 @@ export class MastersComponent implements OnInit {
   deleteobject: any;
   url: string;
   branchForm = new FormGroup({
-    branchName: new FormControl('',Validators.required),
-    branchAddress: new FormControl('',Validators.required),
-    branchEmail: new FormControl('',[Validators.required,Validators.email]),
-    branchPerson: new FormControl('',Validators.required),
-    branchNumber: new FormControl('',Validators.required)
+    branchName: new FormControl('', Validators.required),
+    branchAddress: new FormControl('', Validators.required),
+    branchEmail: new FormControl('', [Validators.required, Validators.email]),
+    branchPerson: new FormControl('', Validators.required),
+    branchNumber: new FormControl('', Validators.required)
   });
   courseForm = new FormGroup({
-    courseName: new FormControl('',Validators.required),
-    courseDescription: new FormControl('',Validators.required)
+    courseName: new FormControl('', Validators.required),
+    courseDescription: new FormControl('', Validators.required)
   });
   userForm = new FormGroup({
-    userName: new FormControl('',Validators.required),
-    userEmail: new FormControl('',[Validators.required,Validators.email]),
-    userPassword: new FormControl('',[Validators.required, Validators.minLength(5)]),
-    userContactPerson: new FormControl('',Validators.required),
-    userContactNumber: new FormControl('',Validators.required)
+    userName: new FormControl('', Validators.required),
+    userEmail: new FormControl('', [Validators.required, Validators.email]),
+    userPassword: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    userContactPerson: new FormControl('', Validators.required),
+    userContactNumber: new FormControl('', Validators.required)
   });
   constructor(
     private service: ApiService,
     private modalService: NgbModal,
     public auth: AuthService,
     private route: Router
-  ) {}
+  ) { }
 
   ngOnInit() {
     if (this.auth.isLoggedIn() !== true) {
@@ -99,7 +99,6 @@ export class MastersComponent implements OnInit {
   }
 
   saveBranchDetails() {
-
     console.log(this.branchForm.value);
     console.log(this.branchForm.status);
     if (this.branchForm.valid) {
@@ -116,31 +115,25 @@ export class MastersComponent implements OnInit {
   }
 
   saveCourseDetails() {
-
     if (this.courseForm.valid) {
       const userId = +this.auth.getUserId();
       this.course.createdBy = userId;
       this.course.updatedBy = userId;
       this.service.post(Constants.course, this.course).subscribe(resp => {
         this.getCourses();
-        this.loading = false; 
+        this.loading = false;
         this.modalReference.close();
       });
     }
-   
   }
 
   saveUserDetails() {
-
     console.log(this.userForm.value);
     console.log(this.userForm.status);
-  
-
   }
 
   onModalClick(content) {
     this.modalReference = this.modalService.open(content);
-    
   }
 
   onDeleteModalClick(content, deleteobject: any, url: string) {
@@ -150,13 +143,26 @@ export class MastersComponent implements OnInit {
   }
   closeModal() {
     this.modalReference.close();
-    
   }
 
   deleteItem() {
     this.service.delete(this.url, this.deleteobject).subscribe(resp => {
       this.getCourses();
+      this.getBranches();
       this.modalReference.close();
     });
+  }
+
+  getbranch(id: number) {
+    this.service.get(Constants.branch + '/' + id).subscribe(resp => {
+      this.bindBranch(resp.data);
+    });
+  }
+  onEditModalClick(content, id) {
+    this.getbranch(id);
+    this.onModalClick(content);
+  }
+  bindBranch(data) {
+    this.branch = data;
   }
 }
