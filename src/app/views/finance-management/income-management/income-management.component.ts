2e01c4fed8ec7bfd10ec9ff8d2a0b0f6896b2ddb
ARUN AG <arun.ag@experionglobal.com>
2019-05-26 14:34:53 +0530

    {s40s} As a user I should be able to record an income transaction

--- src/app/views/finance-management/income-management/income-management.component.ts
+++ src/app/views/finance-management/income-management/income-management.component.ts
@@ -14,17 +14,21 @@ import Swal from 'sweetalert2';
 export class IncomeManagementComponent implements OnInit {
 
   loading: boolean;
+  public btnIncomeSubmited = false;
+  public btnExpenseSubmited = false;
+
   incomeAddForm = new FormGroup({
     incomeHeadCtrl: new FormControl(null, Validators.required),
     descriptionCtrl: new FormControl(null, Validators.required),
     incomeHeadPaymentMode: new FormControl(null, Validators.required),
     incomeHeadDate: new FormControl(null, Validators.required),
-    referenceCtrl: new FormControl(null),
+    referenceCtrl: new FormControl(null, Validators.required),
     amountCtrl: new FormControl(null, [
       Validators.required,
       Validators.pattern('^[0-9]*$'),
       Validators.maxLength(5),
     ]),
+    
   });
   incomeDetails: IncomeDetails = new IncomeDetails();
   incomeHeads: Array<IncomeHead> = new Array<IncomeHead>();
@@ -47,27 +51,31 @@ export class IncomeManagementComponent implements OnInit {
   ResetIncome()
   {
     this.incomeAddForm.reset(); 
+    this.btnIncomeSubmited=false;
   }
   saveIncome()
   {
-    this.incomeDetails.description = this.incomeAddForm.get('descriptionCtrl').value;
-    this.incomeDetails.incomeHeadId = this.incomeAddForm.get('incomeHeadCtrl').value;
-    this.incomeDetails.paymentModeId = this.incomeAddForm.get('incomeHeadPaymentMode').value;
-    this.incomeDetails.transDate = this.incomeAddForm.get('incomeHeadDate').value;
-    this.incomeDetails.reference = this.incomeAddForm.get('referenceCtrl').value;
-    this.incomeDetails.amount = this.incomeAddForm.get('amountCtrl').value;
-    const userId = +this.auth.getUserId();
-    this.incomeDetails.createdBy = userId;
-    this.incomeDetails.updatedBy = userId;
-    this.incomeDetails.branchId=this.auth.getBranchId();
-    console.log(this.incomeDetails); return false;
-    this.loading = true;
-    this.service.post(Constants.incomehead, this.incomeDetails).subscribe(resp => {
-     
-      this.incomeAddForm.reset();
-      this.ShowResponse(resp);
-    });
-  
+    this.btnIncomeSubmited=true;
+    if (this.incomeAddForm.valid){
+      this.incomeDetails.description = this.incomeAddForm.get('descriptionCtrl').value;
+      this.incomeDetails.incomeHeadId = this.incomeAddForm.get('incomeHeadCtrl').value;
+      //this.incomeDetails.paymentModeId = this.incomeAddForm.get('incomeHeadPaymentMode').value;
+      //this.incomeDetails.transDate = this.incomeAddForm.get('incomeHeadDate').value;
+      this.incomeDetails.reference = this.incomeAddForm.get('referenceCtrl').value;
+      this.incomeDetails.amount = this.incomeAddForm.get('amountCtrl').value;
+      const userId = +this.auth.getUserId();
+      this.incomeDetails.createdBy = userId;
+      this.incomeDetails.updatedBy = userId;
+      this.incomeDetails.branchId=this.auth.getBranchId();
+      //console.log(this.incomeDetails); return false;
+      this.loading = true;
+      this.service.post(Constants.income, this.incomeDetails).subscribe(resp => {
+      console.log(resp);
+        this.incomeAddForm.reset();
+        this.ShowResponse(resp);
+      });
+      this.btnIncomeSubmited=false;
+    }
   }
   ShowResponse(response: any) {
     console.log(response);
